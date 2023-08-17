3ccc6a2c5c99d9c065285076740ddcbd577c8b5a
Sofia Bezertzi <sofia_bezertzi@waters.com>
2023-08-18 00:42:21 +0100

    add new hook for coordinates on a map

--- src/hooks/useFetchPropertyLocation.tsx
+++ src/hooks/useFetchPropertyLocation.tsx
@@ -2,33 +2,35 @@ import { useQuery } from "react-query";
 import Axios from "src/api/Axios";
 
 const fetchPropertiesByQuery = (
-    fromDate: string,
-    toDate: string,
     searchLocation: string,
+    lng: number,
+    lat: number,
     region?: string,
 ) => {
     const parameters = region
         ? {
-            from: fromDate,
-            to: toDate,
             city: searchLocation,
+            lng: lng,
+            lat: lat,
             region: region,
         }
         : {
-            from: fromDate, to: toDate, city: searchLocation
+            city: searchLocation,
+            lng: lng,
+            lat: lat
         };
     return Axios.get("/properties", { params: parameters });
 };
 
-export const useSearchResults = (
-    fromDate: string,
-    toDate: string,
+export const usePropertyMapResults = (
     searchLocation: string,
+    lng: number,
+    lat: number,
     region?: string,
 ) => {
     return useQuery({
-        queryKey: [`search-properties`, fromDate, toDate, searchLocation, region],
+        queryKey: [`search-properties`, searchLocation, lng, lat, region],
         queryFn: () =>
-            fetchPropertiesByQuery(fromDate, toDate, searchLocation, region),
+            fetchPropertiesByQuery(searchLocation, lng, lat, region),
     });
 };
