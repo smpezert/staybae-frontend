2b12ff1eb4c3ea58d0157caeb66ad3e1e6cbc172
Sofia Bezertzi <sofia_bezertzi@waters.com>
2023-08-20 17:01:45 +0100

    add markers for the properties on the map

--- src/components/maps/Maps.tsx
+++ src/components/maps/Maps.tsx
@@ -1,29 +1,31 @@
-import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
-import React, { useMemo, useState } from 'react';
+import { GoogleMap, MarkerF, useLoadScript } from '@react-google-maps/api';
+import { useMemo } from 'react';
+import PropertyType from 'src/interfaces/Property';
 
-type Coordinates = {
-    lat: number;
-    lng: number;
-};
 type MapProps = {
-    googleMapsApiKey: string;
+    property: PropertyType[];
 };
 
-const Maps: React.FC = () => {
+const Maps = ({ property }: MapProps) => {
+    console.log(property);
     const { isLoaded } = useLoadScript({
         id: "google-map-script",
         googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY
-    })
-    const center = useMemo(() => ({ lat: 53.457, lng: -2.157 }), [])
-    const [selectedMarker, setSelectedMarker] = useState<Coordinates | null>(center);
+    });
+
+    const center = useMemo(() => ({ lat: 53.457, lng: -2.157 }), []);
 
     if (!isLoaded) return <div>Map is loading ...</div>
 
+    if (!property) {
+        return null;
+    }
 
     return (
-        <div className="flex justify-center p-1 my-2 mx-2 md:my-10">
-            <GoogleMap zoom={12} center={center} mapContainerClassName="flex justify-center h-60 w-80">
-                <Marker position={center} onClick={() => { setSelectedMarker(center); }}></Marker>
+        <div className="flex justify-center p-1 my-2 mx-2">
+            <GoogleMap zoom={3} center={center} mapContainerClassName="h-80 w-full object-cover">
+                {property.map((property, sId) => (
+                    <MarkerF position={{ lat: property.lat, lng: property.lng }} key={sId} />))}
             </GoogleMap>
         </div>
     )
