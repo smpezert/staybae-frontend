c39e4b1a39ba93561342cca0531b8a949e3c1cda
Sofia Bezertzi <sofia_bezertzi@waters.com>
2023-10-21 00:17:58 +0100

    remove comma from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -13,5 +13,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <App />
       </BrowserRouter>
     </FavouriteContextProvider>
-  </React.StrictMode >,
+  </React.StrictMode>
 );
